334ab1c96acf98d4cefc6106e308356367d5a519
Kashish Rahate <114670210+gitrider02@users.noreply.github.com>
2023-11-29 19:00:56 +0530

    Add files via upload

---
+++ assign5.pl
@@ -0,0 +1,16 @@
+# Facts
+female(trisha).
+female(joseph).
+male(rahil).
+male(karim).
+male(kabil).
+parent(rahil,karim).
+parent(rahil,kabil).
+parent(karim,trisha).
+parent(karim,joseph).
+
+#Rules
+son(X,Y):-male(X) ,parent(Y,X).
+daughter(X, Y):- female(X) ,parent(Y ,X).
+sibling(X, Y):-
+parent(Z, X) ,parent(Z,Y), X \= Y.
